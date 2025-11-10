1db9d419e91df4c8c4c546e638476cd321db7bcd
DESKTOP-3FBSC30\HP <msaud.zeesofttech@outlook.com>
2025-11-10 18:25:21 +0500

    User can ser Custom Drawable Background

--- CustomLoadingCard/src/main/java/com/example/customloadingcard/CustomLoadingDialog.kt
+++ CustomLoadingCard/src/main/java/com/example/customloadingcard/CustomLoadingDialog.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,13 +13,15 @@ import androidx.appcompat.app.AppCompatActivity
 class CustomLoadingDialog(val context: Context) {
         private var dialog: Dialog? = null
 
-        fun showCustomDialog(text:String="Loading....",setCancelable: Boolean,loadingIconVisible:Boolean){
+        fun showCustomDialog(text:String="Loading....",setCancelable: Boolean,loadingIconVisible:Boolean,drawables:Int = R.drawable.loading_dialog_bg){
             if (dialog?.isShowing == true) return
 
             //if(dialog?.isShowing == true) return
 
             val view = LayoutInflater.from(context).inflate(R.layout.loading_dialog,null)
             val loadingIcon = view.findViewById<ProgressBar>(R.id.progressBar)
+            val bgChange = view.findViewById<LinearLayout>(R.id.bg_change)
+            bgChange.background = context.getDrawable(drawables)
             loadingIcon.visibility = if(loadingIconVisible) View.VISIBLE else View.GONE
 
             val id = view.findViewById<TextView>(R.id.tvLoading)
