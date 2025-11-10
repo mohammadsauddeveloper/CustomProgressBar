e9320ee9a695353d91f216c8099b6fd101ea1e4b
DESKTOP-3FBSC30\HP <msaud.zeesofttech@outlook.com>
2025-11-10 18:19:30 +0500

    initial commit

---
+++ CustomLoadingCard/src/main/java/com/example/customloadingcard/CustomLoadingDialog.kt
@@ -0,0 +1,44 @@
+package com.example.customloadingcard
+
+import android.app.Dialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class CustomLoadingDialog(val context: Context) {
+        private var dialog: Dialog? = null
+
+        fun showCustomDialog(text:String="Loading....",setCancelable: Boolean,loadingIconVisible:Boolean){
+            if (dialog?.isShowing == true) return
+
+            //if(dialog?.isShowing == true) return
+
+            val view = LayoutInflater.from(context).inflate(R.layout.loading_dialog,null)
+            val loadingIcon = view.findViewById<ProgressBar>(R.id.progressBar)
+            loadingIcon.visibility = if(loadingIconVisible) View.VISIBLE else View.GONE
+
+            val id = view.findViewById<TextView>(R.id.tvLoading)
+            id.text = text
+            dialog = Dialog(context).apply {
+                setContentView(view)
+                setCancelable(setCancelable)
+                setCanceledOnTouchOutside(setCancelable)
+                window?.setBackgroundDrawableResource(android.R.color.transparent)
+                window?.setLayout(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                )
+                show()
+            }
+        }
+
+        fun hide(){
+            dialog?.dismiss()
+            dialog = null
+        }
+
+    }
